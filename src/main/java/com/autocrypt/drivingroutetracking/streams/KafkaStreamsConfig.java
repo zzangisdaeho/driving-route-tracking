16b93e1ef524db9c15c1e57925d640be09331dbb
DaeHo Kim <zzangisdaeho@naver.com>
2024-07-20 21:09:10 +0900

    stop immediately disable

--- src/main/java/com/autocrypt/drivingroutetracking/streams/KafkaStreamsConfig.java
+++ src/main/java/com/autocrypt/drivingroutetracking/streams/KafkaStreamsConfig.java
@@ -5,19 +5,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.SessionStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.util.StringUtils;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,103 +44,67 @@ public class KafkaStreamsConfig {
 
     @Bean
     public KStream<String, String> kStream(StreamsBuilder streamsBuilder) {
-        KStream<String, String> stream = streamsBuilder.stream("gps-topic");
+        KStream<String, String> stream = streamsBuilder.stream("gps-topic", Consumed.with(Serdes.String(), Serdes.String()))
+                .peek((key, value) -> log.info("record income = \n key : {}, value : {}", key, value))
+                .mapValues(KafkaStreamsConfig::parsingRecord);
+//                .map((key, value) -> {
+//                    String newValue = parsingRecord(value);
+//                    return new KeyValue<>(key, newValue);
+//                });
+
 
         KGroupedStream<String, String> groupedStream = stream.groupByKey();
 
-
-        // 상태 저장소를 설정합니다. (예: In-Memory 상태 저장소)
+        // 상태 저장소를 설정
         Materialized<String, String, SessionStore<Bytes, byte[]>> materialized =
                 Materialized.<String, String, SessionStore<Bytes, byte[]>>as(SESSION_STORE_NAME)
                         .withKeySerde(Serdes.String())
                         .withValueSerde(Serdes.String());
 
-
         SessionWindows sessionWindows = SessionWindows.ofInactivityGapWithNoGrace(Duration.ofSeconds(10));
 
         KTable<Windowed<String>, String> aggregatedTable = groupedStream
                 .windowedBy(sessionWindows)
-                .aggregate(
-                        () -> "", // 초기 값
-                        (vehicleId, record, aggregate) -> {
-                            log.info("record income : {}", record);
-                            return parsingRecord(vehicleId, record, aggregate);
-                        },
-                        (aggKey, aggOne, aggTwo) -> aggOne + aggTwo, // 세션 병합 로직
+                .reduce(
+                        (aggValue, newValue) -> aggValue.concat("\n").concat(newValue),
                         materialized
-                );
+                )
+//                .suppress(Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded()))
+                ;
+
 
         aggregatedTable.toStream()
-//                .process(VehicleDataProcessor::new, SESSION_STORE_NAME);
-                .transform(() -> new Transformer<Windowed<String>, String, KeyValue<Windowed<String>, String>>() {
-                    private ProcessorContext context;
-                    private SessionStore<String, String> sessionStore;
-
-                    @Override
-                    public void init(ProcessorContext context) {
-                        this.context = context;
-                        this.sessionStore = context.getStateStore(SESSION_STORE_NAME);
-                    }
-
-                    @Override
-                    public KeyValue<Windowed<String>, String> transform(Windowed<String> key, String value) {
-                        if (value != null && value.contains("STOPPED")) {
-                            processSessionData(key.key(), value);
-                            sessionStore.remove(key);
-                        }
-                        return new KeyValue<>(key, value);
-                    }
-
-                    @Override
-                    public void close() {}
-                }, SESSION_STORE_NAME)
+                .filter((windowedKey, aggregatedValue) -> aggregatedValue!=null && !aggregatedValue.isBlank())
+                .filter((windowedKey, aggregatedValue) -> aggregatedValue.contains("STOP"))
+                .process(VehicleDataProcessor.supplier(), SESSION_STORE_NAME)
                 .foreach((windowedKey, aggregatedValue) -> {
-                    if (aggregatedValue != null && !aggregatedValue.contains("STOPPED")) {
-                        processSessionData(windowedKey.key(), aggregatedValue);
-                    }
+                    log.info("Session start: {}, Session end: {}", Instant.ofEpochMilli(windowedKey.window().start()).atZone(ZoneId.of("Asia/Seoul")), Instant.ofEpochMilli(windowedKey.window().end()).atZone(ZoneId.of("Asia/Seoul")));
+                    log.info("aggregatedValue = \n{}", aggregatedValue);
                 });
 
         return stream;
-
     }
 
-    private static String parsingRecord(String vehicleId, String record, String aggregate) {
-        try {
-            JsonNode jsonNode = objectMapper.readTree(record);
+    private static String parsingRecord(String value) {
 
+        if(value.equalsIgnoreCase("\"STOP\"")) {
+            return "STOP";
+        }
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(value);
             if (jsonNode != null) {
+                String vehicleId = jsonNode.hasNonNull("vehicleId") ? jsonNode.get("vehicleId").asText() : "";
                 String timestamp = jsonNode.hasNonNull("timestamp") ? jsonNode.get("timestamp").asText() : "";
                 double latitude = jsonNode.hasNonNull("latitude") ? jsonNode.get("latitude").asDouble() : 0.0;
                 double longitude = jsonNode.hasNonNull("longitude") ? jsonNode.get("longitude").asDouble() : 0.0;
-                return aggregate + vehicleId + "," + timestamp + "," + latitude + "," + longitude + "\n";
-            } else {
-                return aggregate;
+                return vehicleId + "," + timestamp + "," + latitude + "," + longitude;
             }
         } catch (Exception e) {
-            log.error("record parsing fail : {}", record, e);
-            return aggregate;
+            log.error("Error parsing record: {}", value, e);
         }
+        return "";
     }
 
-    private void processSessionData(String vehicleId, String aggregatedValue) {
 
-        System.out.printf("Vehicle ID: %s, aggregatedValue : %s%n", vehicleId, aggregatedValue);
-
-        // 세션 종료 후 데이터를 처리하는 로직을 구현
-        String[] records = aggregatedValue.split("\n");
-        long currentTime = System.currentTimeMillis();
-        long oneMinuteAgo = currentTime - Duration.ofMinutes(1).toMillis();
-        StringBuilder recentData = new StringBuilder();
-
-        for (String record : records) {
-            String[] parts = record.split(",");
-            long timestamp = Long.parseLong(parts[1]); // assume timestamp is in the second field
-            if (timestamp >= oneMinuteAgo) {
-                recentData.append(record).append("\n");
-            }
-        }
-
-        System.out.printf("Vehicle ID: %s, Recent Data: %s%n", vehicleId, recentData.toString());
-        // 예: 다른 메소드로 전달
-    }
 }
--- src/main/java/com/autocrypt/drivingroutetracking/streams/VehicleDataProcessor.java
+++ src/main/java/com/autocrypt/drivingroutetracking/streams/VehicleDataProcessor.java
@@ -1,65 +1,41 @@
 package com.autocrypt.drivingroutetracking.streams;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.kstream.Windowed;
 import org.apache.kafka.streams.processor.api.Processor;
 import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.ProcessorSupplier;
 import org.apache.kafka.streams.processor.api.Record;
 import org.apache.kafka.streams.state.SessionStore;
-import org.apache.kafka.streams.kstream.Windowed;
-
-import java.time.Duration;
-
-import static com.autocrypt.drivingroutetracking.streams.KafkaStreamsConfig.SESSION_STORE_NAME;
 
 @Slf4j
 public class VehicleDataProcessor implements Processor<Windowed<String>, String, Windowed<String>, String> {
 
     private ProcessorContext<Windowed<String>, String> context;
-    private SessionStore<String, String> sessionStore;
+    private SessionStore<String, String> store;
 
     @Override
     public void init(ProcessorContext<Windowed<String>, String> context) {
         this.context = context;
-        this.sessionStore = context.getStateStore(SESSION_STORE_NAME);
+        this.store = context.getStateStore(KafkaStreamsConfig.SESSION_STORE_NAME);
     }
 
     @Override
     public void process(Record<Windowed<String>, String> record) {
-        log.info("processor received record : {}", record);
-        if (record.value() == null) {
-            log.warn("Received null value for record: {}", record);
-            return;
-        }
-        if (record.value().contains("STOPPED")) {
-            processSessionData(record.key().key(), record.value());
-            context.forward(record.withKey(record.key()).withValue(record.value()));
-            sessionStore.remove(record.key());
-            context.commit();
-        }
-    }
-
-    private void processSessionData(String vehicleId, String aggregatedValue) {
-
-        System.out.printf("Vehicle ID: %s, aggregatedValue : %s%n", vehicleId, aggregatedValue);
-
-        // 세션 종료 후 데이터를 처리하는 로직을 구현
-        String[] records = aggregatedValue.split("\n");
-        long currentTime = System.currentTimeMillis();
-        long oneMinuteAgo = currentTime - Duration.ofMinutes(1).toMillis();
-        StringBuilder recentData = new StringBuilder();
-
-        for (String record : records) {
-            String[] parts = record.split(",");
-            long timestamp = Long.parseLong(parts[1]); // assume timestamp is in the second field
-            if (timestamp >= oneMinuteAgo) {
-                recentData.append(record).append("\n");
-            }
-        }
-
-        System.out.printf("Vehicle ID: %s, Recent Data: %s%n", vehicleId, recentData.toString());
-        // 예: 다른 메소드로 전달
+        String value = record.value();
+        log.info("value processor receive : \n{}", value);
+        Windowed<String> key = record.key();
+        store.remove(key);
+        context.commit();
+        context.forward(record);
     }
 
     @Override
-    public void close() {}
+    public void close() {
+        // 리소스 정리 작업
+    }
+
+    public static ProcessorSupplier<Windowed<String>, String, Windowed<String>, String> supplier() {
+        return VehicleDataProcessor::new;
+    }
 }
